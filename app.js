6b9ee37aaea310d10086532938324a039488a1aa
matrixorz <matrix.orz@gmail.com>
2016-02-02 23:35:16 +0800

    ng-controller不解析{{}}“

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-function BookCtrl($scope){
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('BookCtrl', function BookCtrl($scope){
         $scope.books = [
                 {'name': 'Effective Java', 'author':'Joshua Bloch'},
                 {'name': 'Year without Pants', 'author':'Scott Berkun'},
                 { 'name':'Confessions of public speaker','author':'Scott Berkun'},
                 {'name':'JavaScript Good Parts','author':'Douglas Crockford'}
         ]
-}
+});
