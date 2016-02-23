33947eea58cb05548022406efb80436296497d8b
matrixorz <matrix.orz@gmail.com>
2016-02-24 07:30:27 +0800

    activex learning

--- activex.js
+++ activex.js
@@ -30,3 +30,106 @@ function read_excel(){
     oXL.Quit();
     CollectGarbage();
 }
+function OpenFile()
+{
+try
+{
+FileDialog.CancelError=true;
+FileDialog.Filter="Word模板|*.doc|Word模板|*.dot";
+FileDialog.ShowOpen();
+var WordApp=new ActiveXObject("Word.Application");
+WordApp.Application.Visible=false;
+var Doc=WordApp.Documents.Open(FileDialog.filename);
+Doc.Activate();
+Doc.Parent.Options.InsertedTextColor=4;
+Doc.Parent.Options.InsertedTextMark=2;
+Doc.Parent.Options.DeletedTextColor=4;
+Doc.Parent.Options.DeletedTextMark=1;
+Doc.TrackRevisions=true;
+Doc.PrintRevisions=true;
+Doc.ShowRevisions=true;
+Doc.Application.UserName="";
+var Range=Doc.Range();
+Range.Select();
+var Selection=WordApp.Selection;
+Selection.Copy();
+App.focus();
+document.execCommand("Paste");
+App.focus();
+WordApp.DisplayAlerts=false;
+Doc.Close();
+WordApp.DisplayAlerts=true;
+WordApp.Quit();
+}
+catch(e){}
+return false;
+}
+function wordlean(){
+    //创建ActiveX控件
+    var wrd = new ActiveXObject("Word.Application");
+    //打开服务器word文档
+    wrd.Documents.Open(document.location.href.substring(0,document.location.href.lastIndexOf('/')+1)+'');
+
+    //在段落指定位置写入部门名称和日期
+    var theRange = wrd.ActiveDocument.Range(wrd.ActiveDocument.Paragraphs(2).Range.Start+3,wrd.ActivetheRange.text="技术中心");
+
+    //检索到word中的表格,并在指定单元格里写入内容
+    var theRange = wrd.ActiveXDocument.Tables(1).Rows(i+2).Cells(1).Range;
+    theRange.Text = dataTable.rows(i).cells(0).innerText;
+
+    //自动根据记录数量建立空行再写入内容
+    if (i>0)wrd.Selection.InsertRowsBelow();
+
+    //另存为本地文件
+    Wrd.ActiveDocument.SaveAs('周报'+new Date().getYear()+new DAte().getMonth()+new Date().getDAte()+'.doc');
+    wrd.options.SendMailAttach = true;
+
+    wrd.ActiveDocument.SendMail();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+function html2word(Area){
+    var oWD = new ActiveXObject("Word.Application");
+    var oDC = oWD.Documents.ADD("",0,1);
+    var oRange = oDC.Range(0,1);
+    var sel = document.body.createTextRange();
+    sel.moveToElementText(document.getElementById(Area));
+    sel.select();
+    sel.execCommand("Copy");
+    oRange.Paste();
+    oWd.Application.Visible=true;
+}
